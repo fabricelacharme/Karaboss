759e33c69dd4e3ebb55972ab525f22e3acf4e279
Fabrice Lacharme <fabrice.lacharme@gmail.com>
2025-06-19 13:07:20 +0200

    Version 1.0.10.18

--- Karaboss/Properties/AssemblyInfo.cs
+++ Karaboss/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.10.17")]
-[assembly: AssemblyFileVersion("1.0.10.17")]
+[assembly: AssemblyVersion("1.0.10.18")]
+[assembly: AssemblyFileVersion("1.0.10.18")]
