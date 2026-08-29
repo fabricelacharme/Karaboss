e65bb4a782113586fff79b4b8e83199e1c788c27
Fabrice Lacharme <fabrice.lacharme@gmail.com>
2026-08-29 16:26:02 +0200

    fix

--- FlShell/ShellContextMenu.cs
+++ FlShell/ShellContextMenu.cs
@@ -20,7 +20,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Forms;
 using FlShell.Interop;
 
